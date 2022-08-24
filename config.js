8847ec98213404ea00374714d4ae8b4437d8cf81
miss you <1020814597@qq.com>
2022-08-24 10:53:52 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
   github: {
-    client_id: "Iv1.698a9efc24b74a6a",
-    client_secrets: "25815380b6610e74c3bac519fa56ad02b3b76889",
+    client_id: "tts-Iv1.698a9efc24b74a6a",
+    client_secrets: "tts-25815380b6610e74c3bac519fa56ad02b3b76889",
     request_token_url: 'https://github.com/login/oauth/access_token'
   }
 };
